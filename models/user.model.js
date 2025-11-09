7a3217625ae010b53ed8ea8ca5130a76bcbc3315
Jakub <jakub.sobon2@outlook.com>
2025-11-09 18:03:12 +0100

    initial commit / version error

---
+++ models/user.model.js
@@ -0,0 +1,19 @@
+import { pgTable, uuid, varchar, text, timestamp } from "drizzle-orm/pg-core";
+import { scheduler } from "timers/promises";
+
+export const users = pgTable("users", {
+    id: uuid().primaryKey().defaultRandom(),
+
+    firstname: varchar("firstname", {length: 50}).notNull(),
+    lastname: varchar("lastname", {length: 50}).notNull(),
+
+    email: varchar("email", {length: 100}).notNull().unique(),
+    
+    password: text("password").notNull(),
+    salt: text().notNull(),
+
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").$onUpdate(() => new Date()),
+},{
+    schema: "public",
+});
